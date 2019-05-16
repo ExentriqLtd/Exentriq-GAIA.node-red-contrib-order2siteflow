4fefe6e42ddb542cbfc2a413ac10f6faeab24b3f
Emilio Sabatucci <sabatucci@gmail.com>
2019-05-16 19:21:26 +0200

    fix cover

--- order2siteflow.js
+++ order2siteflow.js
@@ -446,7 +446,7 @@
 		      res.classes.push({
 		        "height": h,
 		        "width": w,
-		        "fit": "fill",
+		        "fit": "cover",
 		        "type": "asset",
 		        "name": itemName,
 		        "asset": itemName
