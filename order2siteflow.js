3c3a5dc5d54c4277f21cfe37faef29acffc377f1
Emilio Sabatucci <sabatucci@gmail.com>
2019-05-24 10:59:32 +0200

    restored cover

--- order2siteflow.js
+++ order2siteflow.js
@@ -471,7 +471,7 @@
 		      res.classes.push({
 		        "height": h,
 		        "width": w,
-		        "fit": "contain",
+		        "fit": "cover",
 		        "type": "asset",
 		        "name": itemName,
 		        "asset": itemName
