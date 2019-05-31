ae4b69049bbc09f5b4d00884bca203525c9ba413
Emilio Sabatucci <sabatucci@gmail.com>
2019-05-31 11:06:18 +0200

    fix

--- order2siteflow.js
+++ order2siteflow.js
@@ -1,5 +1,5 @@
  const OneflowClient = require('oneflow-sdk-js');
- const OneflowClientValidator = require('.node_modules/oneflow-sdk-js/lib/oneflow-validator.js');
+ const OneflowClientValidator = require('./node_modules/oneflow-sdk-js/lib/oneflow-validator.js');
  
  module.exports = function(RED) {
 
